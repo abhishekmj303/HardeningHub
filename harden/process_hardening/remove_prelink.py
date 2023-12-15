794bd6e54f37f9e2e06a189a8111b2d4f7e4963e
chanakya <chanakyavasantha@gmail.com>
2023-12-15 19:11:56 +0530

    remove_prelink added

---
+++ harden/process_hardening/remove_prelink.py
@@ -0,0 +1,20 @@
+import subprocess
+from harden import config_file
+
+def get_script(config):
+    file_systems_config = config["processes"]
+
+    # Start with an empty script and build it up
+    script = ""
+
+    if file_systems_config['remove_prelink']:
+        # Each file system gets its own set of commands
+        script += f"""
+prelink -ua
+apt purge prelink
+"""
+    return script
+
+if __name__ == "__main__":
+    config = config_file.read()
+    print(get_script(config))
