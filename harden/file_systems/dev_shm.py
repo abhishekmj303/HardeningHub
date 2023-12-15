bea53a5b89200b78ed9a687d075e4c7e3b86e67d
chanakya <chanakyavasantha@gmail.com>
2023-12-15 18:51:28 +0530

    dev_shm , tmp python files added, scripts needs to be updated

---
+++ harden/file_systems/dev_shm.py
@@ -0,0 +1,18 @@
+import subprocess
+from harden import config_file
+
+def get_script(config):
+    file_systems_config = config["file-systems"]
+    # Start with an empty script and build it up
+    script = ""
+
+    if file_systems_config['config-fs']['dev_shm']:
+        # Each file system gets its own set of commands
+        script += f"""
+
+"""
+    return script
+
+if __name__ == "__main__":
+    config = config_file.read()
+    print(get_script(config))
