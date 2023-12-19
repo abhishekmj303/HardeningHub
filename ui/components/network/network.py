3707abe1420a39a71050f23602725bf776c1b879
Sri Sujan <mbsrisujan@gmail.com>
2023-12-19 13:46:01 +0530

    add network.py in network page

---
+++ ui/components/network/network.py
@@ -0,0 +1,69 @@
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel, QCheckBox \
+    , QHBoxLayout, QComboBox, QLineEdit
+from harden import config_file
+
+class Net(QWidget):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        self.toml_net = self.config['network']
+        self.init_ui()
+        self.refresh_config(config)
+
+    def init_ui(self):
+        self.layout = QVBoxLayout()
+        self.setLayout(self.layout)
+        self.layout.setSpacing(0)
+        self.layout.setContentsMargins(0, 0, 0, 0)
+
+        self.main_label = QLabel("Network")
+        self.layout.addWidget(self.main_label)
+        self.main_label.setObjectName("component-title")
+
+        # container widget
+        self.container_widget = QWidget()
+        self.container_layout = QVBoxLayout()
+        self.container_widget.setLayout(self.container_layout)
+        self.layout.addWidget(self.container_widget)
+        self.container_layout.setSpacing(0)
+        self.container_layout.setContentsMargins(30, 10, 30, 30)
+        self.container_widget.setObjectName("container-widget")
+
+        self.toml_net_checkboxes = {}
+        for name, state in self.toml_net.items():
+            if name == "disable_protocols":
+                continue
+            checkbox = QCheckBox(f"{name.replace('_',' ').title()}")
+            checkbox.stateChanged.connect(lambda state, name = name: self.save_checkbox_state(name, state))
+            self.toml_net_checkboxes[name] = checkbox
+            self.container_layout.addWidget(checkbox)
+
+        self.protocols_label = QLabel("Disable Protocols")
+        self.container_layout.addWidget(self.protocols_label)
+        self.protocols_label.setObjectName("sub-component-title")
+
+        self.protocols_checkboxes = {}
+        for name, state in self.toml_net['disable_protocols'].items():
+            checkbox = QCheckBox(f"{name.replace('_',' ').title()}")
+            checkbox.stateChanged.connect(lambda state, name=name: self.save_checkbox_state_protocols(state, 'disable_protocols', name))
+            self.container_layout.addWidget(checkbox)
+            self.protocols_checkboxes[name] = checkbox
+
+    def refresh_config(self, config):
+        self.config = config
+        self.toml_net = self.config['network']
+        for name, state in self.toml_net.items():
+            if name == "disable_protocols":
+                continue
+            self.toml_net_checkboxes[name].setChecked(state)
+
+        for name, state in self.toml_net['disable_protocols'].items():
+            self.protocols_checkboxes[name].setChecked(state)
+
+    def save_checkbox_state(self, name, state):
+        self.toml_net[name] = (state == 2)
+        config_file.write(self.config)
+
+    def save_checkbox_state_protocols(self, state, category, name):
+        self.toml_net[category][name] = (state == 2)
+        config_file.write(self.config)
