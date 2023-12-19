4fb316609f5c88284a70d6451e05aba3a5fadf67
chanakya <chanakyavasantha@gmail.com>
2023-12-19 09:59:19 +0530

    App Armor, Aide Added

--- harden/app_armor/app_armor.py
+++ harden/app_armor/app_armor.py
@@ -6,11 +6,13 @@ def get_script(config):
     # Start with an empty script and build it up
     script = ""
 
-    if file_systems_config['enable_aide']:
+    if file_systems_config['enable']:
         # Each file system gets its own set of commands
-        script += f"""
-sudo apt install aide aide-common
-"""
+        script += f"sudo apt install apparmor"
+    if file_systems_config['mode'] == 'enforce':
+        script += f"aa-enforce /etc/apparmor.d/*"
+    else:
+        script += f"aa-complain /etc/apparmor.d/*"
     return script
 
 if __name__ == "__main__":
