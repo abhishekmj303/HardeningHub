7544b483beb545686ff5fd85ef3720614e825df0
chanakya <chanakyavasantha@gmail.com>
2023-12-20 14:31:09 +0530

    fire wal enable and commands updated

--- harden/firewall.py
+++ harden/firewall.py
@@ -4,19 +4,19 @@ def get_script(config):
     firewall_config = config["firewall"]
     script = "#!/bin/bash\n\n"  # Start with a bash shebang and a newline
 
-    if firewall_config.get('enable', False):
+    if firewall_config['enable']:
         script += "sudo apt install ufw -y\n"  # Install UFW
         script += "sudo apt purge iptables-persistent -y\n"  # Purge iptables-persistent
         script += "sudo systemctl enable --now ufw.service\n"  # Enable and start UFW
 
-        if firewall_config.get('configure_loopback_traffic', False):
+        if firewall_config['configure_loopback_traffic']:
             # Configure loopback traffic rules
             script += "sudo ufw allow in on lo\n"
             script += "sudo ufw allow out on lo\n"
             script += "sudo ufw deny in from 127.0.0.0/8\n"
             script += "sudo ufw deny in from ::1\n"
         
-        if firewall_config.get('enable_default_deny', False):
+        if firewall_config['enable_default_deny']:
             # Set default deny policies
             script += "sudo ufw default deny incoming\n"
             script += "sudo ufw default deny outgoing\n"
