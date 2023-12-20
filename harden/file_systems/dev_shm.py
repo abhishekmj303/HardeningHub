843944da4fee36d150adbe9b271dec2739476763
chanakya <chanakyavasantha@gmail.com>
2023-12-20 08:08:46 +0530

    Audits Working

--- harden/file_systems/dev_shm.py
+++ harden/file_systems/dev_shm.py
@@ -8,11 +8,34 @@ def get_script(config):
 
     if file_systems_config['configure_fs']['dev_shm']:
         # Each file system gets its own set of commands
-        script += f"""
+        script += """
+#!/bin/bash
+
+echo "Applying remediation for the /dev/shm partition..."
+
+# Backup /etc/fstab
+cp /etc/fstab /etc/fstab.backup
+
+# Add nodev option to /dev/shm in /etc/fstab
+if grep -q "/dev/shm" /etc/fstab; then
+    # /dev/shm is present in fstab, update it
+    sed -i '/\/dev\/shm/ s/defaults/defaults,nodev/' /etc/fstab
+else
+    # /dev/shm not present, add it
+    echo "tmpfs /dev/shm tmpfs defaults,nodev 0 0" >> /etc/fstab
+fi
+
+# Remount /dev/shm
+mount -o remount /dev/shm
+
+echo "Remediation applied. /dev/shm is now mounted with 'nodev' option."
 
 """
     return script
 
 if __name__ == "__main__":
-    config = config_file.read()
+    config = config_file.init()
     print(get_script(config))
+
+
+
