4a864b4c5d1b41154541e0c44fa4364bba9b7667
chanakya <chanakyavasantha@gmail.com>
2023-12-20 14:53:26 +0530

    time Sync commands updated, enable added

--- harden/time_sync.py
+++ harden/time_sync.py
@@ -10,13 +10,13 @@ def get_script(config):
         # Install NTP package
         script += "sudo apt install ntp\n"
         # Add or edit the line in /etc/init.d/ntp
-        if file_systems_config['ntp_servers']:
-            for item in file_systems_config['ntp_servers']:
-                script += f"echo 'server {item}' iburst| sudo tee -a /etc/ntp.conf\n"
-        if file_systems_config['enable_ntp_user']:
-            script += "echo 'RUNASUSER=ntp' | sudo tee -a /etc/init.d/ntp\n"
+        if file_systems_config['enable_ntp_server']:
+            if file_systems_config['ntp_servers']:
+                for item in file_systems_config['ntp_servers']:
+                    script += f"echo 'server {item}' iburst| sudo tee -a /etc/ntp.conf\n"
+            if file_systems_config['enable_ntp_user']:
+                script += "echo 'RUNASUSER=ntp' | sudo tee -a /etc/init.d/ntp\n"
     
-
         # Restart NTP service
         script += "sudo systemctl restart ntp.service\n"
     
